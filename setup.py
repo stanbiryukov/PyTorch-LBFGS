5e2744134018c7403174ae60bbe0d1c01ff6dea4
stanbiryukov <stan0625@uw.edu>
2021-02-07 16:42:38 -0600

    pip compat

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("PyTorch-LBFGS/version.py") as f:
+    exec(f.read())
+
+extra_setuptools_args = dict(
+    tests_require=['pytest']
+)
+
+setup(
+    name ='PyTorch-LBFGS',
+    version = __version__,
+    author ='Hao-Jun Michael Shi (Northwestern University) and Dheevatsa Mudigere (Facebook)',
+    last_edited_by = 'Stan Biryukov'
+    url = 'git@github.com:stanbiryukov/PyTorch-LBFGS.git',
+    install_requires = requirements,
+    package_data = {'PyTorch-LBFGS':['resources/*']},
+    packages = find_packages(exclude=['PyTorch-LBFGS/tests']),
+    license = 'MIT',
+    description='PyTorch-LBFGS: A PyTorch Implementation of L-BFGS',
+    long_description= "PyTorch-LBFGS is a modular implementation of L-BFGS, a popular quasi-Newton method, for PyTorch that is compatible with many recent algorithmic advancements for improving and stabilizing stochastic quasi-Newton methods and addresses many of the deficiencies with the existing PyTorch L-BFGS implementation. It is designed to provide maximal flexibility to researchers and practitioners in the design and implementation of stochastic quasi-Newton methods for training neural networks.",
+    keywords = ['statistics','matlab','pytorch', 'python', 'lbfgs', 'wolfe'],
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License"
+    ],
+    **extra_setuptools_args
+)
